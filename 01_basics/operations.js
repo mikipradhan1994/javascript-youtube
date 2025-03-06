e36a4b6ebc6e4e648d9ddd5a3b5cfe9f88cc08db
mikipradhan1994 <pradhanmiki93@gmail.com>
2025-03-06 14:42:44 +0000

    comparison operation and operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,34 @@
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2 + "2")
+// console.log("2" + 2)
+// console.log(1 + 2 + "2")
+// console.log("1" + 2 + 2)
+// console.log((2 * 3) + 5 / 2) // always use parenthesis
+
+// console.log(null); // null
+// console.log(undefined)
+
+// console.log(true);
+// console.log(+true)
+
+let counter = 100;
+++counter
+//console.log(++2)
+//console.log(+"") //0
+//console.log(counter);
+
+
+/***********************************comparison operation******************************************/
+
+console.log("2" > 1)
+console.log("2" < 1)
+console.log(null > 0)
+console.log(null >= 0)
+console.log(null == 0)
+
+console.log(undefined > 0)
+console.log(undefined >= 0)
+console.log(undefined == 0)
