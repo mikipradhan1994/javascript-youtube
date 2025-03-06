2e8cf7950555dc7bc5a9d93cfe29dc9f4293333b
mikipradhan1994 <pradhanmiki93@gmail.com>
2025-03-06 11:52:10 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all js code as newer version.
+/*
+string => ""
+number => 2 to power 53 (range)
+bigint
+boolean => true/false
+null => it is standalone value
+undefined => declared variable but the value is not assigned.
+symbol => unique
+object
+*/
+
+console.log(typeof "miki");
+
