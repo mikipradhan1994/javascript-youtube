ff01c13f70985c74b87935bae922d8a41d53f238
mikipradhan1994 <pradhanmiki93@gmail.com>
2025-03-06 15:09:31 +0000

    datatype summery

--- 01_basics/dataTypes.js
+++ 01_basics/dataTypes.js
@@ -10,5 +10,31 @@ symbol => unique
 object
 */
 
-console.log(typeof "miki");
+//console.log(typeof "miki");
+
+/*  DATA TYPE SUMMERY */
+
+// Primitive data type
+
+/* 7type
+   ---------
+   String, Number, Null, Undefined, Boolean,symbol, BigInt
+*/
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); //false
+
+const bigNumber = 3456543554655n
+
+/*Non primitive or reference data type
+Array, Objects, functions
+
+*/
 
