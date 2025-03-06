0bec837b7376f7f6f11d34d5f35720d79d129e14
mikipradhan1994 <pradhanmiki93@gmail.com>
2025-03-06 12:08:57 +0000

    conversion datatype

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,26 @@
+let value = 33;
+
+let numberConversion = Number(value)
+console.log(value)
+console.log(typeof(vale));
+console.log(conversion)
+
+/*
+33 => 33
+"33abc" => NaN
+true => 1, false=> 0
+*/
+
+let isLoggedIn = "miki";
+let booleanConversion = Boolean(isLoggedIn);
+
+/*
+1 => true, 0=> false
+"" => false
+"miki"=> true
+*/
+let number = 33
+let stringConversion = String(number)
+
+console.log(stringConversion);
+console.log(typeof stringConversion);
