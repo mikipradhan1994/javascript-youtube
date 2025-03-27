5286b310f731c7d22f518a1e72a3782a97f8462f
mikipradhan1994 <pradhanmiki93@gmail.com>
2025-03-27 07:08:30 +0000

    promise asyn await

--- 01_basics/promise.js
+++ 01_basics/promise.js
@@ -1,44 +1,87 @@
-const promiseOne = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log("Promise one created");
-        resolve()
-    }, 1000)
-})
+// const promiseOne = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log("Promise one created");
+//         resolve()
+//     }, 1000)
+// })
 
-promiseOne.then(() => {
-    console.log("Promise one consumed");
-})
+// promiseOne.then(() => {
+//     console.log("Promise one consumed");
+// })
 
-new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log("Promise one created");
-        resolve()
-    }, 1000)
-}).then(() => {
-    console.log("Promise one consumed");
-})
+// new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log("Promise one created");
+//         resolve()
+//     }, 1000)
+// }).then(() => {
+//     console.log("Promise one consumed");
+// })
 
-const promiseTwo = new Promise((resolve, reject) => {
-    let error = false;
-    if(!error) {
-        resolve({name: "Miki", password: "123f"})
-    } else {
-        reject("Error: Something went wrong")
-    }
-})
+// const promiseTwo = new Promise((resolve, reject) => {
+//     let error = false;
+//     if(!error) {
+//         resolve({name: "Miki", password: "123f"})
+//     } else {
+//         reject("Error: Something went wrong")
+//     }
+// })
 
-promiseTwo.then((user) => {
-    console.log(user);
-    return user.name
-}).then((myName) => {
-    console.log(myName);
-}).catch((error) => {
-    console.log(error)
-}).finally(() => {
-    console.log("Finally prmise is either resolve or reject")
-})
+// promiseTwo.then((user) => {
+//     console.log(user);
+//     return user.name
+// }).then((myName) => {
+//     console.log(myName);
+// }).catch((error) => {
+//     console.log(error)
+// }).finally(() => {
+//     console.log("Finally prmise is either resolve or reject")
+// })
 
-const promiseFive = new Promise((resolve,reject) => {
+// const promiseFive = new Promise((resolve,reject) => {
+//     setTimeout(() => {
+//         let error = false;
+//         if(!error) {
+//             resolve({name: "Miki", Age: 30, Address: "Odisha"});
+//         } else {
+//             reject("ERROR: Something went wrong");
+//         }
+//     }, 1000)
+// })
+
+// const consumePromiseFive = async() => {
+//     try {
+//         const response = await promiseFive;
+//         console.log(response) 
+//     } catch (error) {
+//         console.log(error)
+//     }
     
+// }
+
+// consumePromiseFive();
+
+
+
+// you can write async function using normal function or using arrow function
+// async function consumePromiseFive(){}
+// const getUsersData = async() => {
+//     try {
+//         const response = await fetch("https://api.github.com/users/mikipradhan1994");
+//          const data = await response.json()
+//         console.log(data.login)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// getUsersData();
+
+fetch("https://api.github.com/users/mikipradhan1994")
+.then((response) => {
+    return response.json()
+}).then((data) => {
+    console.log(data)
 })
+
 
