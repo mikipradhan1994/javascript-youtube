383c724ce0d21ede19c6498c538429b28c0a0b1f
mikipradhan1994 <pradhanmiki93@gmail.com>
2025-04-03 11:03:27 +0000

    create store and reducer

---
+++ reductToolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,29 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const initialState = {
+        todos: [{
+        id: 1,
+        text: "Hello World"
+    }]
+}   
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid,
+                text: action.payload
+            }
+            state.todo.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== todo.payload)
+        }
+    }
+})
+
+export const {addTodo, removeTodo} = todoSlice.actions
+
+export default todoSlice.reducer
